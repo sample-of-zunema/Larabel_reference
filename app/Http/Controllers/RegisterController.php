5404216aca96b6450f1d0455b21d8a8fbeb298a4
zunema <yxxa.abc@gmail.com>
2022-03-12 19:37:00 +0900

    クラス更新

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -8,7 +8,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
-class RegisterController extends Controllers
+class RegisterController extends Controller
 {
   public function create()
   {
