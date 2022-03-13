f56a986a78055de7609919604fbbe96aecb77f14
zunema <yxxa.abc@gmail.com>
2022-03-13 23:25:11 +0900

    login.blade.phpログインフォーム作成

--- routes/web.php
+++ routes/web.php
@@ -34,4 +34,3 @@ Route::get('/login', [App\Http\Controllers\LoginController::class, 'index'])
     ->name('login')
 Route::post('/login', [App\Http\Controllers\LoginController::class, 'authenticate'])
     ->middleware('guest')
-
