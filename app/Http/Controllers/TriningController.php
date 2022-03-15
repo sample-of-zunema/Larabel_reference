6ff569472fd0c5d10cf0fdd9530cfa5775f78069
zunema <yxxa.abc@gmail.com>
2022-03-16 00:50:01 +0900

    TriningController作成

---
+++ app/Http/Controllers/TriningController.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TriningController extends Controller
+{
+  public function tri()
+  {
+    echo '練習成功！！'；
+  }
+}
