82dfd48d6dc2259a5fac0c1dc7e79f1e2032b18f
zunema <yxxa.abc@gmail.com>
2022-03-23 00:26:50 +0900

    authors更新

--- database/migrations/2022_03_22_150847_create_authors_table.php
+++ database/migrations/2022_03_22_150847_create_authors_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
+            $table->string('name','100');
+            $table->string('kana','100');
             $table->timestamps();
         });
     }
