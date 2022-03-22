a941f5bc688374d9a53b6ea0ff1d1a5e51484f73
zunema <yxxa.abc@gmail.com>
2022-03-22 23:55:40 +0900

    ミドルウェア更新

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict=types=1);
+
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
@@ -14,6 +16,7 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
+        \App\Http\Middleware\HeaderDumper::class,
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
