ae0c96f4dab5b4427d032eed7e74c072af795958
zunema <yxxa.abc@gmail.com>
2022-03-25 22:07:32 +0900

    DBファサードを利用したクエリビルダの取得

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -18,4 +18,16 @@ class Book extends Model
     {
         return $this->belongsTo('\App\Models\Author');
     }
+
+    // 5.4.2.1. DBファサードを利用したクエリビルダの取得
+    // DBファサードからBooksテーブルのクエリビルダを取得
+    $query = \Illuminate\Support\Facades\DB::table('books');
+
+    // 5.4.2.2. Connectionクラスからクエリビルダを取得
+    // 1. サービスコンテナからDatabaseManagerクラスのインスタンスを取得
+    $db = \Illuminate\Foundation\Application::getInstance()->make('db');
+    // 2. 上記インスタンスからConnectionクラスのインスタンスを取得
+    $connection = $db->connection();
+    // 3. 上記インスタンスからクエリビルダを取得
+    $query = $connection->table('books');
 }
