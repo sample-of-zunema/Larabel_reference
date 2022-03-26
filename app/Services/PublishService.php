e5d10f10f13c498c80c0b1befacbf1e1b6cffd4a
zunema <yxxa.abc@gmail.com>
2022-03-27 01:29:36 +0900

    サービスクラスのリファクタリング

--- app/Services/PublishService.php
+++ app/Services/PublishService.php
@@ -3,27 +3,53 @@
 
 namespace App\Services;
 
-use App\DataProvider\Eloquent\Publisher;
+// 5.5.3.4. サービスクラスのリファクタリング　App\DataProvider\Eloquent\Publisher　→　App\DataProvider\PublisherRepositoryInterface　に変更
+// use App\DataProvider\Eloquent\Publisher;
+use App\DataProvider\PublisherRepositoryInterface;
+use App\Domain\Entity\Publisher;
+
 
 class PublisherService
 {
+  // // 5.5.3.4. リポジトリ使用のためコメントアウトする
+  // public function exists(string $name): bool
+  // {
+  //   $count = Publisher::whereName($name)->count();
+  //   if ($count > 0) {
+  //     return true;
+  //   }
+  //   return false;
+  // }
+
+  // public function store(string $name, string $address): int
+  // {
+  //   $publisher = Publisher::create(
+  //     [
+  //       'name' => $name,
+  //       'address' => $address,
+  //     ]
+  //   );
+  //   return (int)$publisher->id;
+  // }
+
+  // 5.5.3.4. にて追加
+  private $publisher;
+
+  public function __construct(PublisherRepositoryInterface $publisher)
+  {
+    $this->publisher = $publisher;
+  }
+
   public function exists(string $name): bool
   {
-    $count = Publisher::whereName($name)->count();
-    if ($count > 0) {
-      return true;
+    if (!$this->publisher->findByName($name)) {
+      return false;
     }
-    return false;
+    return true;
   }
 
   public function store(string $name, string $address): int
   {
-    $publisher = Publisher::create(
-      [
-        'name' => $name,
-        'address' => $address,
-      ]
-    );
-    return (int)$publisher->id;
+    return $this->publisher->store(new Publisher(null, $name, $address));
   }
 }
