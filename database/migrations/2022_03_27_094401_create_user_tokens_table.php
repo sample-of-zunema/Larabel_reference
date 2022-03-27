f57462a0adc4df84bb9ac4509771751c3957df40
zunema <yxxa.abc@gmail.com>
2022-03-27 18:57:25 +0900

    user_tokensテーブる更新

--- database/migrations/2022_03_27_094401_create_user_tokens_table.php
+++ database/migrations/2022_03_27_094401_create_user_tokens_table.php
@@ -4,7 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+// return new class extends Migration
+// 6.2.1.3. user_tokensテーブル作成
+class CreateUserTokensTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +16,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('user_tokens', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->integer('user_id', false, true);
+            $table->string('api_token', 60)->unique();
+            $table->timestamps('created_at')
+                ->useCurrentOnUpdate();
+            $table->foreign('user_id')->references('id')
+                on('users')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
