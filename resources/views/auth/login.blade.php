c9493a30695e5550eaefb29fd282520e2ff677ba
yuya iio <65650872+zunema@users.noreply.github.com>
2023-01-19 22:43:29 +0900

    fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,8 +12,8 @@
   <form name="loginform" action="/login" method="post">
     {{ csrf_field() }}
     <dl>
-      <dt>メールアドレス：</dt><dd><input type="text" name="email" size="30" value="{{ old('email') }}"></dd>
-      <dt>パスワード：</dt><dd><input type="password" name="password" size="30"></dd>
+      <dt>メールアドレス： </dt><dd><input type="text" name="email" size="30" value="{{ old('email') }}"></dd>
+      <dt>パスワード： </dt><dd><input type="password" name="password" size="30"></dd>
     </dl>
     <button type='submit' name='action' value='send'>ログイン</button>
   </form>
